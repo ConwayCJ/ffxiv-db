05e0b342b65edb7b6cf640069f596753e86721f8
ConwayCJ <christopherjconway07@gmail.com>
2024-07-15 00:36:44 -0700

    adjust CLI command info

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ def main():
   subparsers = parser.add_subparsers(dest="command", help="AVAILABLE COMMANDS")
 
   # SINGLE ARG COMMANDS
-  subparsers.add_parser('new-craft-db', help='Converts ffxiv wikipedia to CSV files for each profession.')
   subparsers.add_parser('list-professions', help="1-9, 10-19, 20-29, 30-39, 40-49, 50,\n51-59, 60, 61-69, 70, 71-79, 80, 81-89, 90")
-  subparsers.add_parser('make-crafter-csv', help="Parses the ffxiv wikipedia and converts every professions craftable items a single csv file")
+  subparsers.add_parser('make-crafter-csv', help="Generates a CSV file for every profession")
 
   # MULTIPLE ARG COMMANDS
-  comm_parse_page = subparsers.add_parser('make-profession-csv', help="Parses the ffxiv wikipedia and converts a single professios craftable items to a single csv file. e.g. <main.py make-profession-csv blacksmith>")
+  comm_parse_page = subparsers.add_parser('make-profession-csv', help="Generates a CSV file for a single profession. e.g. <main.py make-profession-csv blacksmith>")
   comm_parse_page.add_argument('profession', type=str, help='Name of the profession')  
 
   args = parser.parse_args()
