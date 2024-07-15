860797995c0b80d094a18991b96f5eb989d78b8d
ConwayCJ <christopherjconway07@gmail.com>
2024-07-15 00:00:42 -0700

    feat: add fnc to convert all crafters db to csv

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -2,7 +2,26 @@ from html_to_craftables import parse_all_pages, parse_page, rows_to_craftables, 
 from html_to_craftables import Craftable, Ingredient, professions, tiers
 import csv
 
-# doesn't use a dict
+def make_crafter_db_csv():
+  header = ["name", "profession", "png", "lvl", "type", "yield", "dur", "diff", "max_qual", "ingredients", "range"]
+
+  with open('crafter_db.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    
+    # write header
+    writer.writerow(header)
+    dict = parse_all_pages()
+    
+    # each profession
+    for prof in dict:
+    # each range
+      for range in dict[prof]:
+    # each craftable
+        for item in dict[prof][range]:
+          print(item)
+          vals = item.vals_to_list()
+          writer.writerow(vals + [range])
+
 def make_profession_csv(prof): 
 
   header = ["name", "profession", "png", "lvl", "type", "yield", "dur", "diff", "max_qual", "ingredients", "range"]
